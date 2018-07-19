c8fcf25d67afd64aec017dd010f0207d9e0794b8
Matthew Yates <mattyat3s+bit7186@gmail.com>
2018-07-19 07:11:06 +0100

    Fix

--- auto-deployer/index.js
+++ auto-deployer/index.js
@@ -233,7 +233,6 @@ function validateRequest (req) {
  * @param {Object} res Cloud Function response context.
  */
 exports.githubAutoDeployer = (req, res) => {
-  console.log(req.body.repository);
   const user = req.body.repository.owner.login;
   const repo = req.body.repository.name;
   const repository = `${user}/${repo}`;
